35d85a32acf662b54803ad2e97a3917b92274974
sunmiaou <130472899+sunmiaou@users.noreply.github.com>
2026-02-24 20:31:48 +0100

    (modif): rangement des classes dans leur fichier perso

---
+++ game-engine/classes/cards/card.ts
@@ -0,0 +1,13 @@
+class Card {
+    name:       string;
+    text:       string; 
+    img:        string;
+    type:       string;
+
+    constructor() {
+        this.name = "DefaultName";
+        this.text = "DefaultDesc";
+        this.type = "DefaultType";
+        this.img = "Link";
+    }
+}
