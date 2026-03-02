86e64b1e7703d2d8df45bb7390977719c567ca2f
sunmiaou <130472899+sunmiaou@users.noreply.github.com>
2026-03-02 15:30:18 +0100

    (ajout) classe carte poke fini (sauf les attaques / talents)
    c'etait fait y'a longtemps jsp pourquoi c'etait pas push

--- game-engine/classes/cards/pokemoncard.ts
+++ game-engine/classes/cards/pokemoncard.ts
@@ -1,20 +1,22 @@
 class PokemonCard extends Card {
-    health:     		number;
-    stage:      		number;
-    element:    		string;
-	status:				string;
-	special:			string;
+    private health:     number;
+    private stage:      number;
+    private element:    string;
+	private status:		string;
+	private special:	string;
 	/** Special == EX, Mega EX etc, pour interaction Mega Signal etc */
-	rulebox:			boolean;
-	prize:				number;
-	/** WIP TALENT AND ATTACKS */
-    abilty:     		string;
-	attack1:     		string;
-	attack2:     		string;
-	weakness:			string;
-	resist:				string;
-	retreat:			number;
+	private rulebox:	boolean;
+	private prize:		number;
+	/** WIP TALENT AND ATTACKS 
+	 *  Sera remplace par une classe specifique*/
+    private slot1:     	string;
+	private slot2:     	string;
 
+	private weakness:	string;
+	private resist:		string;
+	private retreat:	number;
+
+	/** Constructeurs */
 	constructor() {
 		super();
 		this.health = 0;
@@ -24,11 +26,48 @@ class PokemonCard extends Card {
 		this.special = "Default";
 		this.rulebox = false;
 		this.prize	= 1;
-		this.abilty = "None";
-		this.attack1 = "DefaultAttack";
-		this.attack2 = "DefaultAttack";
+		this.slot1= "DefaultAttack";
+		this.slot2 = "DefaultAttack";
 		this.weakness = "None";
 		this.resist = "None";
 		this.retreat = 0;
+	}
+
+	/** Getters */
+	getHealth(): number {
+		return this.health;
+	}
+	getStage(): number {
+		return this.stage;
+	}
+	getElement(): string {
+		return this.element;
+	}
+	getStatus(): string { 
+		return this.status;
+	}
+	getSpecial(): string {
+		return this.special;
+	}
+	isRulebox(): boolean {
+		return this.rulebox;
+	}
+	getPrize(): number {
+		return this.prize;
+	}
+	getSlot1(): string {
+		return this.slot1;
+	}
+	getSlot2(): string {
+		return this.slot2;
+	}
+	getWeakness(): string {
+		return this.weakness;
+	}
+	getResist(): string {
+		return this.resist;
+	}
+	getRetreat(): number {
+		return this.retreat;
 	}
 }
