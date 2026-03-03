2b236e11358f5f801626a9c47715010844e9213e
Corentin Fleuret <cf.fleuret@gmail.com>
2026-03-03 19:44:33 +0100

    (modif) : code valide

--- websockets/src/index.ts
+++ websockets/src/index.ts
@@ -1,6 +1,7 @@
-const express = require('express');
+import express from 'express'
+import http from 'http'
+
 const app = express();
-const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
