abb857791dfb9fd474ca5ca72689e817b7d063f0
Corentin Fleuret <cf.fleuret@gmail.com>
2026-03-04 16:57:36 +0100

    (ajout) : tests avec socket.io dans next

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,6 +1,39 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { socket } from "../../socket";
+
 export default function ProfilePage() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
   return (
     <div className="min-h-screen flex flex-col relative overflow-hidden">
       <div 
@@ -11,6 +44,11 @@ export default function ProfilePage() {
           animation: 'gradient-shift 15s ease infinite'
         }}
       />
+      Sockets tests ici
+      <div>
+        <p>Status: { isConnected ? "connected" : "disconnected" }</p>
+        <p>Transport: { transport }</p>
+      </div>
       <style jsx>{`
         @keyframes gradient-shift {
           0% {
