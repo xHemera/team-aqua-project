eee59be3aeed3e7452fce395cea1b13ae4e0220c
Corentin Fleuret <cf.fleuret@gmail.com>
2026-03-04 15:45:54 +0100

    (ajout) : debut d'un serveur avec des websockets

--- websockets/src/index.ts
+++ websockets/src/index.ts
@@ -1,11 +1,27 @@
 import express from 'express'
 import http from 'http'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { Server } from "socket.io";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {}
+});
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () => {
