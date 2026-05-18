211de2f8fcb52684f50ec08f3d9b7ee49f7e3bab
sunmiaou <130472899+sunmiaou@users.noreply.github.com>
2026-05-18 15:10:01 +0200

    (modif) update de spellregistry

--- engine/Spells/SpellRegistry.ts
+++ engine/Spells/SpellRegistry.ts
@@ -1,3 +1,5 @@
+import { archer } from "../../frontend/public/gameResources/heroes/archer/hero";
+import { assassin } from "../../frontend/public/gameResources/heroes/assassin/hero";
 import { PiercingShot, PrecisionFocus, RainOfArrows } from "./archer-spells";
 import { PhantomStep, ShadowStrike, VenomBlade } from "./assassin-spells";
 import { Spell } from "./Spell";
@@ -5,12 +7,12 @@ import { Spell } from "./Spell";
 type SpellFactory = () => Spell;
 
 const SPELL_REGISTRY: Record<string, SpellFactory> = {
-	"archer_s1": () => new PiercingShot(),
-	"archer_s2": () => new RainOfArrows(),
-	"archer_s3": () => new PrecisionFocus(),
-    "assassin_s1": () => new ShadowStrike(),
-    "assassin_s2": () => new VenomBlade(),
-    "assassin_s3": () => new PhantomStep(),
+	"archer_s1": () => new PiercingShot(archer.skills[0].scaling),
+	"archer_s2": () => new RainOfArrows(archer.skills[1].scaling),
+	"archer_s3": () => new PrecisionFocus(archer.skills[2].scaling),
+    "assassin_s1": () => new ShadowStrike(assassin.skills[0].scaling),
+    "assassin_s2": () => new VenomBlade(assassin.skills[1].scaling),
+    "assassin_s3": () => new PhantomStep(assassin.skills[2].scaling),
 };
 
 export function buildSpellMap(characterId: string, skills: { id: string }[]): Map<string, Spell> {
