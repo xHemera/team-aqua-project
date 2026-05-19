4a81fafd28cc1b006ebb3ac41dddb4e8a10ebcee
sunmiaou <130472899+sunmiaou@users.noreply.github.com>
2026-05-19 17:42:27 +0200

    (add) Arcane Missiles

--- engine/Spells/mage-spells.ts
+++ engine/Spells/mage-spells.ts
@@ -1,7 +1,31 @@
-import { trace } from "console";
 import { CharacterInstance } from "../Instances/CharacterInstance";
 import { Spell } from "./Spell";
 import { resolveMagDamage } from "../Utils/resolveDamage";
+
+export class ArcaneMissiles extends Spell{
+	constructor(scaling: number[][]) {
+		super(scaling);
+		this.id			= "s2";
+		this.name		= "Arcane Missiles";
+		this.mpCost		= 16;
+		this.targeting	= "aoe";
+	}
+
+	applyEffect(idUser: CharacterInstance, idTargets: CharacterInstance[]): void {
+		const skillLevel = idUser.character.skills.find(s => s.id === this.id)?.level ?? 1;
+		const [missileCount, multiplier] = this.scaling[skillLevel - 1];
+		const raw = idUser.character.stats.magicalDamage * multiplier;
+
+		for (let i = 0; i < missileCount; i++) {
+        const livingTargets = idTargets.filter(t => t.currentHp > 0);
+        if (livingTargets.length === 0) break;
+
+        const target = livingTargets[Math.floor(Math.random() * livingTargets.length)];
+        const damage = resolveMagDamage(raw, idUser, target);
+        target.currentHp = Math.max(0, target.currentHp - damage);
+    }
+	}
+}
 
 export class Meteor extends Spell{
 	constructor(scaling: number[][]) {
