dd06c198e9307012947f83513492bd0745682497
Corentin Fleuret <cf.fleuret@gmail.com>
2026-05-20 13:12:23 +0200

    (ajout) : api pour admin maj

--- frontend/app/api/characters/route.ts
+++ frontend/app/api/characters/route.ts
@@ -111,72 +111,72 @@ export async function GET(req: Request)
 export async function POST(req: Request)
 {
   const h = await headers();
-    const ip = h
-    .get("x-forwarded-for")
-    ?.split(",")[0]
-    .trim() || "unknown";
+  const ip = h
+  .get("x-forwarded-for")
+  ?.split(",")[0]
+  .trim() || "unknown";
 
-    const allowed = await rateLimit(redis, `rl:characters${ip}`, 5, 1);
+  const allowed = await rateLimit(redis, `rl:characters${ip}`, 5, 1);
 
-    if (!allowed) {
-        console.log("Too many requests");
-        return Response.json({error: "Too many request"}, {status: 429});
-    }
-    try {
-      const data = await req.json();
-      const { name } = data;
-      const userId = await prisma.user.findFirst({
-        where: { name: name },
-        select: {
-          id: true,
-          gameState: true,
-        }
-      });
-      if (!userId)
-        return Response.json({error: "Internal server error"}, {status: 500});
+  if (!allowed) {
+      console.log("Too many requests");
+      return Response.json({error: "Too many request"}, {status: 429});
+  }
+  try {
+    const data = await req.json();
+    const { name } = data;
+    const userId = await prisma.user.findFirst({
+      where: { name: name },
+      select: {
+        id: true,
+        gameState: true,
+      }
+    });
+    if (!userId)
+      return Response.json({error: "Internal server error"}, {status: 500});
 
-      const gs = await prisma.gameState.create({
+    const gs = await prisma.gameState.create({
+      data: {
+        user_id: userId.id,
+        rubis: 0,
+      }
+    });
+    if (!gs)
+      return Response.json({error: "Internal server error"}, {status: 500});
+
+    for (const character of CHARACTERS)
+    {
+      const char = await prisma.character.create({
         data: {
-          user_id: userId.id,
-          rubis: 0,
+          gameStateId: gs.id,
+          name: character.identity.name,
+          hp: character.baseStats.hp,
+          level: 1,
+          xp: 0,
         }
-      });
-      if (!gs)
+      })
+      if (!char)
         return Response.json({error: "Internal server error"}, {status: 500});
-
-      for (const character of CHARACTERS)
+      for (const spell of character.skills)
       {
-        const char = await prisma.character.create({
+        await prisma.spell.create({
           data: {
-            gameStateId: gs.id,
-            name: character.identity.name,
-            hp: character.baseStats.hp,
+            characterId: char.id,
+            name: spell.info.name,
+            type: spell.type,
+            mpCost: spell.manaCost,
             level: 1,
             xp: 0,
           }
-        })
-        if (!char)
-          return Response.json({error: "Internal server error"}, {status: 500});
-        for (const spell of character.skills)
-        {
-          await prisma.spell.create({
-            data: {
-              characterId: char.id,
-              name: spell.info.name,
-              type: spell.type,
-              mpCost: spell.manaCost,
-              level: 1,
-              xp: 0,
-            }
-          });
-        }
+        });
       }
-      return Response.json({msg: "Created"}, {status: 201});
     }
-    catch (e) {
-      console.log(e);
-      return Response.json({error: "Internal server error"}, {status: 500});
-    }
+    return Response.json({msg: "Created"}, {status: 201});
+  }
+  catch (e) {
+    console.log(e);
+    return Response.json({error: "Internal server error"}, {status: 500});
+  }
 }
 
 export async function PUT(req: Request)
