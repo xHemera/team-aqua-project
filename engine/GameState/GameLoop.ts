43539eb0717035826f854fd32e948d15b149215d
sunmiaou <130472899+sunmiaou@users.noreply.github.com>
2026-05-22 15:38:29 +0200

    (add) loop pour frontend

---
+++ engine/GameState/GameLoop.ts
@@ -0,0 +1,30 @@
+import { getCurrentTurnCharacter, initGame } from "../GameEngine";
+import { GameState } from "./GameState"
+
+export type GameLoopState = {
+	gameState:			GameState;
+	activePlayerOwner:	number;
+	activeCharacterUid:	string;
+	waitingForAction:	boolean;
+	winner?:			number;
+}
+
+export class GameLoop {
+	private state: GameState;
+
+	constructor(initialState: GameState) {
+		this.state = initGame(initialState);
+	}
+
+	getLoopState(): GameLoopState {
+		const activeCharacter = getCurrentTurnCharacter(this.state);
+
+		return {
+		gameState:          this.state,
+		activePlayerOwner:  activeCharacter?.owner ?? 0,
+		activeCharacterUid: activeCharacter?.uid   ?? "",
+		waitingForAction:   this.state.gamePhase === "battle",
+		winner:             this.state.winnerId,
+		};
+	}
+}
