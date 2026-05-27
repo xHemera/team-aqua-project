911e62018010389eabd669a27f85c589e816315c
Corentin Fleuret <cf.fleuret@gmail.com>
2026-05-27 16:26:51 +0200

    (ajout) : gestion de l'url socket via .env

--- frontend/socket.js
+++ frontend/socket.js
@@ -3,7 +3,10 @@
 import { io } from "socket.io-client";
 
 //connects the socket to the server and sets some options
-export const socket = io("http://localhost:4001", {
+
+const url = process.env.SOCKET ?? "http://localhost:4001";
+
+export const socket = io(url, {
   transports: ["websocket", "polling"],
   autoConnect: false,
   reconnection: true
