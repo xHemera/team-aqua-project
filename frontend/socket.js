adb0c7d110544595fc40f003834df0ecd1662228
Corentin Fleuret <cf.fleuret@gmail.com>
2026-05-28 12:08:19 +0200

    (ajout) : correction socket via env

--- frontend/socket.js
+++ frontend/socket.js
@@ -4,10 +4,10 @@ import { io } from "socket.io-client";
 
 //connects the socket to the server and sets some options
 
-const url = process.env.SOCKET ?? "http://localhost:4001";
+const url = process.env.NEXT_PUBLIC_SOCKET ?? "http://localhost:4001";
+console.log(JSON.stringify(url));
 
-export const socket = io(url, {
-  transports: ["websocket", "polling"],
+export const socket = io(url.trim(), {
   autoConnect: false,
-  reconnection: true
+  reconnection: true,
 });
